52244cad8a5758be26e5bced0c6293fd1e1837a0
InnaOs <R_musavarov@mail.ru>
2022-05-10 22:39:24 +0300

    java_core_6

---
+++ Clouds.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Clouds{
+    Integer all;
+
+    public Clouds() {
+    }
+
+    public Integer getAll() {
+        return all;
+    }
+
+    public void setAll(Integer all) {
+        this.all = all;
+    }
+
+}
